44424862bea80da0e7a21c9801533e52d3d5b683
RmanShch <man46ro@gmail.com>
2022-04-29 16:23:09 +0300

    realm installed

---
+++ Where'sMyMoney/Where'sMyMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Where'sMyMoney
+//
+//  Created by Анна Шербицкая on 29.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
